74b4085363c898dd309887607bebf172e92dc127
Istergul <istergul@gmail.com>
2012-07-25 10:59:31 +0400

    added dynamic examples

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
-console.log('This would be the main JS file.');
+$(document).ready(function() {
+
+    $('#sortableSimple, #sortableMultiple, #sortableSections').tableDnD({
+        onDragClass: "myDragClass",
+        checkableClass: "trCheck",
+        onDragStart: function(table, draggedRow, dragObjs) { },
+        onDrop: function(table, droppedRow, dragObjs) { }
+    });
+
+});
+
